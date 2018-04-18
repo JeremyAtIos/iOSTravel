4959fa2d8016d23630d44ab0d7f43cbece25b8a9
肖伟 <1097526615@qq.com>
2018-04-18 22:40:03 +0800

    ADD AttributedText project

---
+++ AttributedText/AttributedText/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  AttributedText
+//
+//  Created by Metallic  on 2018/4/18.
+//  Copyright © 2018年 xiaowei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+    }
+
+    // 1. 不同位置，不同文字颜色
+    // 2. 垂直偏移量
+    // 3. 文字间距
+    // 4. 删除线
+    // 5. 下划线
+    // 6. 字体
+    // 7. 加粗
+    // 8. 斜体
+}
+
